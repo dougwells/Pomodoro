f3910501eb95a2046224d1c09be704944dbad86c
dougwells <dougwells@gmail.com>
2016-02-06 15:51:49 -0700

    Timer logic works.  Simple & Elegant solution

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,8 +1,20 @@
-// Doug Wells's Calculator project
-// See if you can find the "easter egg"
-// Finding it is as easy as PI
 
 $(document).ready(function(){
+              $('#circle-2').circliful();
 
+var timer = 2;
+var timerSeconds = timer*60;
+var minutes = Math.floor(timerSeconds/60);
+var seconds = timerSeconds - minutes*60;
+
+
+//hit start button //
+
+// setInterval(function(){
+//   timerSeconds = timerSeconds-1;
+//   minutes = Math.floor(timerSeconds/60);
+//   seconds = timerSeconds - minutes*60;
+//   console.log(minutes, seconds);
+// }, 500);
 
 });
