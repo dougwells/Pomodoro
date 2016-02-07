10d4d6e3c87cfd356dd32c571022aae4d715c2ab
dougwells <dougwells@gmail.com>
2016-02-07 16:25:26 -0700

    Still working on radial progress moving w/time

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,16 @@
+//Press start
+
+//while loop
+  //timer starts.
+  //work = 0-100%
+  //play = 101% - 200%
+  //done = 201%
+    //run function that returns an object
+        //countdown.percent
+        //countdown.percent-color (light grn to dark grn)
+        //countdown.mode/message (work, play or done)
+        //countdown.mode-color (progress bar color)
+
 $( document ).ready(function() {
 
 //Initial variables set by User
@@ -14,24 +27,15 @@ $( document ).ready(function() {
 
 //Object to hold timer output & properties
   var timer = {};
+  var i = 0;
 
-//Press start
 
-//while loop
-  //timer starts.
-  //work = 0-100%
-  //play = 101% - 200%
-  //done = 201%
-    //run function that returns an object
-        //countdown.percent
-        //countdown.percent-color (light grn to dark grn)
-        //countdown.mode/message (work, play or done)
-        //countdown.mode-color (progress bar color)
 //
   console.log("Before Loop.  Work Min = " + workMin);
-  while (elapsedSec<endSec){
+  // while (i<10){
     setInterval(function(){
       elapsedSec = elapsedSec + 1;
+      console.log(elapsedSec);
 
       //work time
       if(elapsedSec<=workSec){
@@ -40,33 +44,43 @@ $( document ).ready(function() {
         timer.percent = elapsedSec/workSec*100;
         timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
         timer.elementColor = "aqua";
+        console.log(timer.percent);
 
         //playtime
       }else if (elapsedSec<endSec){
           remainingSec = playSec + workSec - elapsedSec;
           timer.message = "Go Play ...!";
-          timer.percent = (elapsedSec-workSec/playSec*100;
+          timer.percent = (elapsedSec-workSec)/playSec*100;
           timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
           timer.elementColor = "aqua";
 
         //if work time is over, set play timer
         }else {
           remainingSec = 0;
+          timer.message = "Another Pomodoro?";
+          timer.percent = 0;
+          timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
+          timer.elementColor = "aqua";
         }
+        i=i+1;
         timer.minutes = Math.floor(remainingSec/60);
-        timer.seconds = remainingSec - minutes*60;  //function, returns timer object
-        console.log(minutes, seconds);
-    },500);
-  }
+        timer.seconds = remainingSec - timer.minutes*60;  //function, returns timer object
+        console.log(timer.minutes, timer.seconds);
+        $("#circle-1").attr("data-part", timer.percent);
+        $('#circle-1').circliful();
+    return timer.percent;
+  }, 100);
+    console.log("loop complete.  Minutes = " + timer.percent);
+  // }
 
 
 
   //Input data into radial progress indicator
 
-    $("#circle-1").attr("data-text", "55%");
-    $("#circle-1").attr("data-part", playMin);
-  	$("#circle-1").attr("data-total", workMin);
-    $('#circle-1').circliful();
+    // $("#circle-1").attr("data-text", "55%");
+    // $("#circle-1").attr("data-part", playMin);
+  	// $("#circle-1").attr("data-total", workMin);
+
 
 
 });
