1ac163ec6f58213e6ac5f7ac34f07148b789caab
dougwells <dougwells@gmail.com>
2016-02-08 13:01:24 -0700

    Timer stops after work and play

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,15 +1,3 @@
-//Press start
-
-//while loop
-  //timer starts.
-  //work = 0-100%
-  //play = 101% - 200%
-  //done = 201%
-    //run function that returns an object
-        //countdown.percent
-        //countdown.percent-color (light grn to dark grn)
-        //countdown.mode/message (work, play or done)
-        //countdown.mode-color (progress bar color)
 
 $( document ).ready(function() {
 
@@ -29,14 +17,14 @@ $( document ).ready(function() {
   var timer = {};
   var i = 0;
   var j = 0;
+  var k = 0;
 
 
 //
   console.log("Before Loop.  Work Min = " + workMin);
-  // while (i<10){
+  // while (elapsedSec<endSec){
     setInterval(function(){
       elapsedSec = elapsedSec + 1;
-      // console.log(elapsedSec);
 
       //work time
       if(elapsedSec<=workSec){
@@ -45,13 +33,12 @@ $( document ).ready(function() {
         timer.percent = Math.floor(elapsedSec/workSec*100);
         timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
         timer.elementColor = "aqua";
-        // console.log(timer.percent);
 
         //playtime
       }else if (elapsedSec<endSec){
           remainingSec = playSec + workSec - elapsedSec;
           timer.message = "Go Play ...!";
-          timer.percent = (elapsedSec-workSec)/playSec*100;
+          timer.percent = Math.floor(elapsedSec-workSec)/playSec*100;
           timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
           timer.elementColor = "aqua";
 
@@ -66,16 +53,16 @@ $( document ).ready(function() {
 
         timer.minutes = Math.floor(remainingSec/60);
         timer.seconds = remainingSec - timer.minutes*60;  //function, returns timer object
-        if(timer.seconds==50) {
-          i=i+1;
-          j=i.toString()+"%";
+        if(timer.seconds==0 && elapsedSec<=endSec) {
+          i=timer.percent;
+          j=timer.percent.toString()+"%";
           console.log(remainingSec, j, timer.minutes, timer.seconds, timer.percent);
-          $('#circle-1').empty().removeData().attr('data-percent', j).circliful()
           $("#circle-1").attr("data-text", j);
           $("#circle-1").attr("data-part", i);
-          $('#circle-1').circliful();
-            }
-  }, 25);
+          $("#circle-1").attr("data-info", timer.message);
+          $('#circle-1').empty().removeData().attr('data-percent', j).circliful()
+        }
+  }, 5);
   // }
 
 
