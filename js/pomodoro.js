6af94b1b2295b065a9b6d5bfec9af95a663d90fc
dougwells <dougwells@gmail.com>
2016-02-08 08:46:59 -0700

    Radial Progress bar working/updating ...

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -14,7 +14,7 @@
 $( document ).ready(function() {
 
 //Initial variables set by User
-  var workMin = 20;
+  var workMin = 50;
   var playMin = 10;
 
 //Other variables set at start
@@ -28,6 +28,7 @@ $( document ).ready(function() {
 //Object to hold timer output & properties
   var timer = {};
   var i = 0;
+  var j = 0;
 
 
 //
@@ -35,16 +36,16 @@ $( document ).ready(function() {
   // while (i<10){
     setInterval(function(){
       elapsedSec = elapsedSec + 1;
-      console.log(elapsedSec);
+      // console.log(elapsedSec);
 
       //work time
       if(elapsedSec<=workSec){
         remainingSec= workSec - elapsedSec;
         timer.message = "Time to work ...";
-        timer.percent = elapsedSec/workSec*100;
+        timer.percent = Math.floor(elapsedSec/workSec*100);
         timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
         timer.elementColor = "aqua";
-        console.log(timer.percent);
+        // console.log(timer.percent);
 
         //playtime
       }else if (elapsedSec<endSec){
@@ -62,24 +63,29 @@ $( document ).ready(function() {
           timer.bgColor = "linear-gradient(to bottom right, #7FDBFF, #0074D9)"
           timer.elementColor = "aqua";
         }
-        i=i+1;
+
         timer.minutes = Math.floor(remainingSec/60);
         timer.seconds = remainingSec - timer.minutes*60;  //function, returns timer object
-        console.log(timer.minutes, timer.seconds);
-        $("#circle-1").attr("data-part", timer.percent);
-        $('#circle-1').circliful();
-    return timer.percent;
-  }, 100);
-    console.log("loop complete.  Minutes = " + timer.percent);
+        if(timer.seconds==50) {
+          i=i+1;
+          j=i.toString()+"%";
+          console.log(remainingSec, j, timer.minutes, timer.seconds, timer.percent);
+          $('#circle-1').empty().removeData().attr('data-percent', j).circliful()
+          $("#circle-1").attr("data-text", j);
+          $("#circle-1").attr("data-part", i);
+          $('#circle-1').circliful();
+            }
+  }, 25);
   // }
 
 
 
   //Input data into radial progress indicator
 
-    // $("#circle-1").attr("data-text", "55%");
-    // $("#circle-1").attr("data-part", playMin);
-  	// $("#circle-1").attr("data-total", workMin);
+    // $("#circle-1").attr("data-text", "60%");
+    // $("#circle-1").attr("data-part", 30);
+  	// $("#circle-1").attr("data-total", 100);
+    // $('#circle-1').circliful();
 
 
 
