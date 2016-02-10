507804811cc24924efa929f6a28fb6ce1079948a
dougwells <dougwells@gmail.com>
2016-02-10 05:43:06 -0700

    Cleaned up code (eliminated redundant code & improved comments.  Really like my code presentation in pomodoro.js

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -2,8 +2,11 @@
 $( document ).ready(function() {
 
     $('#circle-1').circliful();
-    pomodoro(1,1);
+    pomodoro(0.5,0.25);
 
+
+
+// Pomodoro Function
 
 function pomodoro (workMin, playMin){
 //variables set at start
@@ -39,7 +42,7 @@ function pomodoro (workMin, playMin){
           timer.fgColor = "#FDFC47";
           timer.elementColor = "linear-gradient(to bottom right, #24FE41, #FDFC47)";
 
-        //if work time is over, set play timer
+        //When work & play time are over, reset pomodoro
         }else {
           remainingSec = 0;
           timer.message = "Another Pomodoro?";
@@ -50,44 +53,38 @@ function pomodoro (workMin, playMin){
         }
 
 
-        //Format for cicliful & for app users.  Circliful draws radial progress bar
+        //  Input fields needed for cicliful & user display elements
+        //  Circliful draws radial progress bar
         timer.minutes = Math.floor(remainingSec/60);
         timer.seconds = remainingSec - timer.minutes*60;
         (timer.seconds<10 ? timer.ss = "0"+timer.seconds.toString() : timer.ss=timer.seconds.toString());
         timer.mmss = timer.minutes.toString() + ":" + timer.ss;
 
         //Update time & progress bar until both work & play time runs out
-        if(elapsedSec<endSec) {
+        $("#circle-1").attr("data-text", timer.mmss);
+        $("#circle-1").attr("data-part", timer.percent);
+        $("#circle-1").attr("data-info", timer.message);
+        //Change color of radial progress bar (depending on work or play)
+        $("#circle-1").attr("data-bgcolor", timer.bgColor);
+        $("#circle-1").attr("data-fgcolor", timer.fgColor);
 
-          console.log(remainingSec, timer.mmss, timer.minutes, timer.seconds, timer.percent);
-          $("#circle-1").attr("data-text", timer.mmss);
-          $("#circle-1").attr("data-part", timer.percent);
-          $("#circle-1").attr("data-info", timer.message);
-
-          //work time HTML page background color
-          $("#circle-1").attr("data-bgcolor", timer.bgColor);
-          $("#circle-1").attr("data-fgcolor", timer.fgColor);
+      //Set HTML background color at start of work time, start of play & at end
+        if(elapsedSec<endSec){
           if(elapsedSec===1){$("body").css("background", "linear-gradient(to bottom right, #00dbde, #fc00ff");}
-
-          //playtime HTML page background color when work time is done and play time has begun
-          $("#circle-1").attr("data-bgcolor", timer.bgColor);
-          $("#circle-1").attr("data-fgcolor", timer.fgColor);
           if(elapsedSec===workSec){$("body").css("background", "linear-gradient(to bottom right, #FDFC47, #24FE41");}
-          $('#circle-1').empty().removeData().attr('data-part', timer.percent).circliful();
-
-        }else {
-
-            clearInterval(clock);
-            console.log("done ", remainingSec, timer.mmss, timer.minutes, timer.seconds, timer.percent);
-            $("#circle-1").attr("data-text", timer.mmss);
-            $("#circle-1").attr("data-part", timer.percent);
-            $("#circle-1").attr("data-info", timer.message);
-            $("#circle-1").attr("data-bgcolor", timer.bgColor);
-            $("#circle-1").attr("data-fgcolor", timer.fgColor);
-            $("body").css("background", "linear-gradient(to bottom right, #7FDBFF, #0074D9");
-            $('#circle-1').empty().removeData().attr('data-part', timer.percent).circliful()
+          console.log(remainingSec, timer.mmss, timer.minutes, timer.seconds, timer.percent);
+        } else {
+          //Need to clear setInterval so timer stops running at end of work and play
+          clearInterval(clock);
+          $("body").css("background", "linear-gradient(to bottom right, #7FDBFF, #0074D9");
+          console.log("done ", remainingSec, timer.mmss, timer.minutes, timer.seconds, timer.percent);
         }
-  }, 200);
+
+      //Update & redraw radial progress bar each second.  Search "github circliful" for more information
+        $('#circle-1').empty().removeData().attr('data-part', timer.percent).circliful();
+
+  }, 200);  //End "clock" / setInterval
+
 } // End pomodoro function
 
 }); // End $(document).ready
